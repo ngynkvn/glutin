886c6d78aa24b15d06e6e01f500c2040008de4c5
Kevin Nguyen <ngynkvn@gmail.com>
2021-08-03 19:35:43 -0400

    playtesting

--- glutin_examples/examples/window.rs
+++ glutin_examples/examples/window.rs
@@ -1,38 +1,102 @@
+#[macro_use]
+extern crate glium;
+
 mod support;
 
-use glutin::event::{Event, WindowEvent};
-use glutin::event_loop::{ControlFlow, EventLoop};
-use glutin::window::WindowBuilder;
-use glutin::ContextBuilder;
+use ::glutin::dpi::LogicalSize;
+#[allow(unused_imports)]
+use glium::{glutin, Surface};
+use glium::{index::PrimitiveType, Program};
+use glium_glyph::{
+    glyph_brush::{ab_glyph::FontArc, Layout, Section, Text, VerticalAlign},
+    GlyphBrush,
+};
+
+use std::{
+    borrow::BorrowMut,
+    time::{Duration, Instant},
+};
+// The `TextSystem` contains the shaders and elements used for text display.
 
 fn main() {
-    let el = EventLoop::new();
-    let wb = WindowBuilder::new().with_title("A fantastic window!");
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new().with_inner_size(LogicalSize::new(800.0, 600.0));
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let windowed_context = ContextBuilder::new().build_windowed(wb, &el).unwrap();
+    let dejavu = FontArc::try_from_slice(include_bytes!("../../f.ttf")).unwrap();
+    let fonts = vec![dejavu];
 
-    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+    let mut glyph_brush = GlyphBrush::new(&display, fonts);
 
-    println!("Pixel format of the window's GL context: {:?}", windowed_context.get_pixel_format());
+    println!("hello.");
+    #[derive(Copy, Clone, Debug)]
+    struct Vertex {
+        position: [f32; 2],
+        color: [f32; 3],
+    }
 
-    let gl = support::load(&windowed_context.context());
+    let data = [
+        Vertex { position: [-0.5, -0.5], color: [0.0, 1.0, 0.0] },
+        Vertex { position: [0.0, 0.5], color: [0.0, 0.0, 1.0] },
+        Vertex { position: [0.5, -0.5], color: [1.0, 0.0, 0.0] },
+    ];
 
-    el.run(move |event, _, control_flow| {
-        println!("{:?}", event);
-        *control_flow = ControlFlow::Wait;
+    implement_vertex!(Vertex, position, color);
+    // building the vertex buffer, which contains all the vertices that we will draw
+    let vertex_buffer = { glium::VertexBuffer::new(&display, &data).unwrap() };
+    // building the index buffer
+    let index_buffer =
+        glium::IndexBuffer::new(&display, PrimitiveType::TrianglesList, &[0u16, 1, 2]).unwrap();
+    let program =
+        Program::from_source(&display, include_str!("vert.vs"), include_str!("frag.fs"), None)
+            .unwrap();
 
-        match event {
-            Event::LoopDestroyed => return,
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::Resized(physical_size) => windowed_context.resize(physical_size),
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                _ => (),
-            },
-            Event::RedrawRequested(_) => {
-                gl.draw_frame([1.0, 0.5, 0.7, 1.0]);
-                windowed_context.swap_buffers().unwrap();
+    let mut draw = move || {
+        // building the uniforms
+        let uniforms = uniform! {
+            matrix: [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0f32]
+            ]
+        };
+
+        // drawing a frame
+        let mut target = display.draw();
+        target.clear_color(1.0, 1.0, 1.0, 1.0);
+        target
+            .draw(&vertex_buffer, &index_buffer, &program, &uniforms, &Default::default())
+            .unwrap();
+        {
+            let s = data.iter().map(|v| (format!("{:?}\n", v.position), v)).collect::<Vec<_>>();
+            println!("{:?}", s);
+            for (s, v) in s.iter() {
+                let section = Section::default()
+                    .add_text(Text::new(s))
+                    .with_screen_position((v.position[0], v.position[1]));
+                glyph_brush.queue(section);
             }
-            _ => (),
+            glyph_brush.draw_queued(&display, &mut target);
         }
+        target.finish().unwrap();
+    };
+    draw();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = match event {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                // Break from the main loop when the window is closed.
+                glutin::event::WindowEvent::CloseRequested => glutin::event_loop::ControlFlow::Exit,
+                // Redraw the triangle when the window is resized.
+                glutin::event::WindowEvent::Resized(..) => {
+                    draw();
+                    glutin::event_loop::ControlFlow::Poll
+                }
+                _ => glutin::event_loop::ControlFlow::Poll,
+            },
+            _ => glutin::event_loop::ControlFlow::Poll,
+        };
     });
 }
